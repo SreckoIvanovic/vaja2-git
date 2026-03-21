6adf455b9d149492412fcfcc00daeef44f1eabee
Srecko Ivanovic <srecko.ivanovic@student.um.si>
2026-03-21 16:50:15 +0100

    Implementiral branje stevil iz vhodne datoteke

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+// readNumbers reads numbers from a file
+bool readNumbers(const string& fileName, vector<unsigned char>& numbers) {
+    ifstream in(fileName.c_str());
+    if (!in.is_open()) {
+        return false;
+    }
+
+    int value;
+    while (in >> value) {
+        numbers.push_back(static_cast<unsigned char>(value));
+    }
+
+    in.close();
+    return true;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        return 1;
+    }
+
+    string inputFile = argv[1];
+    vector<unsigned char> numbers;
+
+    if (!readNumbers(inputFile, numbers)) {
+        return 1;
+    }
+
+    return 0;
+}
